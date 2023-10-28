e664a5dc56aa3b7c2680cd2a2bd43b08dc3c9410
JD <jayprowings@gmail.com>
2023-10-28 12:44:17 +0530

    first commit

---
+++ src/main/java/com/prowings/service/MyService.java
@@ -0,0 +1,16 @@
+package com.prowings.service;
+
+public class MyService {
+	
+	
+	public void doSomething()
+	{
+		System.out.println("inside doSomething of MyService class!!");
+	}
+
+	public void doSomething(int i)
+	{
+		System.out.println("inside doSomething(int i) of MyService class!!");
+	}
+
+}
