64e4063c56ac464cf24f665d23dd53096b781f93
JD <jayprowings@gmail.com>
2023-10-31 09:53:07 +0530

    added student class and updated method from service

--- src/main/java/com/prowings/service/MyService.java
+++ src/main/java/com/prowings/service/MyService.java
@@ -8,9 +8,9 @@ public class MyService {
 		System.out.println("inside doSomething of MyService class!!");
 	}
 
-	public void doSomething(int i, int j, float k)
+	public void doSomething(int i, float j, long k)
 	{
-		System.out.println("inside doSomething(int i) of MyService class!!");
+		System.out.println("inside doSomething(int i, float j, long k) of MyService class!!");
 	}
 //test git
 }
